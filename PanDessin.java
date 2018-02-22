db45a746b3d1c2ddb9e1c06c205391eddf42ba61
Émile Brunelle <ebrunelle08@gmail.com>
2018-02-22 16:10:35 -0500

    Ajout de l'antialiasing

--- PanDessin.java
+++ PanDessin.java
@@ -2,6 +2,8 @@ package EmileBrunelle_PatrickPapineau_TP1_Graphique;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -36,6 +38,11 @@ public class PanDessin extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		RenderingHints rh = new RenderingHints(
+	             RenderingHints.KEY_TEXT_ANTIALIASING,
+	             RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2.setRenderingHints(rh);
 		for (Forme forme : liste) {
 			forme.tracer(g);
 		}
