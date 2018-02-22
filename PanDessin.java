aa76cd47cbefcc6e261d809d259da7823d4887ef
Émile Brunelle <ebrunelle08@gmail.com>
2018-02-22 11:35:31 -0500

    Update de PanDessin

--- PanDessin.java
+++ PanDessin.java
@@ -1,6 +1,9 @@
 package EmileBrunelle_PatrickPapineau_TP1_Graphique;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
@@ -10,6 +13,48 @@ public class PanDessin extends JPanel {
 	Color Fg, Bg;
 
 	private class MouvementSourisListener implements MouseListener, MouseMotionListener {
+
+		@Override
+		public void mouseDragged(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
 
 	}
 }
