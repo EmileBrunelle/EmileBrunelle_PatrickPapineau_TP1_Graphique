0bd2aa4e0bb9d2eca3347d2151590c32b12efa2b
Patrick Papineau <patpap002gmail.com>
2018-03-01 11:42:14 -0500

    ajoute de GestionBarreMenu
    un nouveau ActionListener pour la classe BarreMenu

--- BarreMenu.java
+++ BarreMenu.java
@@ -1,6 +1,9 @@
 package EmileBrunelle_PatrickPapineau_TP1_Graphique;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -19,21 +22,26 @@ public class BarreMenu extends JMenuBar {
 
 		nouveau = new JMenuItem("Nouveau");
 		nouveau.setMnemonic(KeyEvent.VK_N);
+		nouveau.addActionListener(new GestionBarreMenu());
 		menuFichier.add(nouveau);
 
 		enregistrer = new JMenuItem("Enregistrer");
 		enregistrer.setMnemonic(KeyEvent.VK_E);
+		enregistrer.addActionListener(new GestionBarreMenu());
 		menuFichier.add(enregistrer);
 
 		enregistrersous = new JMenuItem("Enregistrer-sous");
+		enregistrersous.addActionListener(new GestionBarreMenu());
 		menuFichier.add(enregistrersous);
 
 		ouvrir = new JMenuItem("Ouvrir");
 		ouvrir.setMnemonic(KeyEvent.VK_O);
+		ouvrir.addActionListener(new GestionBarreMenu());
 		menuFichier.add(ouvrir);
 
 		quitter = new JMenuItem("Quitter");
 		quitter.setMnemonic(KeyEvent.VK_Q);
+		quitter.addActionListener(new GestionBarreMenu());
 		menuFichier.add(quitter);
 
 		add(menuFichier);
@@ -43,10 +51,40 @@ public class BarreMenu extends JMenuBar {
 		afficherAide = new JMenuItem("Afficher l'aide");
 		menuAide.add(afficherAide);
 
-		aPropos = new JMenuItem("À propos de Dessin Vectoriel");
+		aPropos = new JMenuItem("A propos de Dessin Vectoriel");
+		aPropos.addActionListener(new GestionBarreMenu());
 		menuAide.add(aPropos);
 
 		add(menuAide);
+
+	}
+	
+	private class GestionBarreMenu implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String indButton = e.getActionCommand();
+			System.out.println(indButton);
+	        switch (indButton) {
+	            case "Nouveau":  ;
+	                     break;
+	            case "Enregistrer": ;
+	                     break;
+	            case "Enregistrer-sous":  ;
+	                     break;
+	            case "Ouvrir":  ;
+	                     break;
+	            case "Quitter":  ;
+	                     break;
+	            case "A propos de Dessin Vectoriel":  ;
+	                     break;
+
+	            default: System.out.println("Default case");
+	                     break;
+	        }
+			
+			
+		}
 
 	}
 
